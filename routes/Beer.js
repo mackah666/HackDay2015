5cea808424a23a04515df9dc401a06382a62e3b1
michael <michael@android-efb7fe84cf90eded.home>
2015-06-20 15:56:01 +0100

    Added beer route

---
+++ routes/Beer.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+var mongoose = require('mongoose');
+var Todo = require('../models/beer.js');
+
+/* GET /todos listing. */
+router.get('/', function(req, res, next) {
+  Todo.find(function (err, todos) {
+    if (err) return next(err);
+    res.json(todos);
+  });
+});
+
+/* GET /todos/id */
+router.get('/:id', function(req, res, next) {
+  Todo.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+
+/* POST /todos */
+router.post('/', function(req, res, next) {
+  Todo.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+module.exports = router;
