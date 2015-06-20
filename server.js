a24d0b03c8e2a36495f9448b024eaf95e6455446
michael <michael@michaels-Air.home>
2015-06-20 15:00:15 +0100

    added mongo lab config

--- server.js
+++ server.js
@@ -4,13 +4,49 @@ var mongoose = require('mongoose');
 // Load required packages
 var Beer = require('./models/beer');
 var bodyParser = require('body-parser');
+var uriUtil = require('mongodb-uri');
 
 
 
-mongoose.connect('mongodb://localhost:27017/beerlocker');
+/* 
+ * Mongoose by default sets the auto_reconnect option to true.
+ * We recommend setting socket options at both the server and replica set level.
+ * We recommend a 30 second connection timeout because it allows for 
+ * plenty of time in most operating environments.
+ */
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };       
+ 
+/*
+ * Mongoose uses a different connection string format than MongoDB's standard.
+ * Use the mongodb-uri library to help you convert from the standard format to
+ * Mongoose's format.
+ */
+var mongodbUri = 'mongodb://hacker:hacker@ds031098.mongolab.com:31098/mackahmarket';
+var mongooseUri = uriUtil.formatMongoose(mongodbUri);
+ 
+mongoose.connect(mongooseUri, options);
+var conn = mongoose.connection;             
+ 
+conn.on('error', console.error.bind(console, 'connection error:'));  
+ 
+conn.once('open', function() {
+  // Wait for the database connection to establish, then start the app.                         
+});
+
 
 // Create our Express application
 var app = express();
+
+// Use the body-parser package in our application
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+
+
+
 
 // Use environment defined port or 3000
 var port = process.env.PORT || 3000;
@@ -23,6 +59,35 @@ var router = express.Router();
 router.get('/', function(req, res) {
   res.json({ message: 'You are running dangerously low on beer, really!' });
 });
+
+
+
+// -- New Code Below Here -- //
+
+// Create a new route with the prefix /beers
+var beersRoute = router.route('/beers');
+
+// Create endpoint /api/beers for POSTS
+beersRoute.post(function(req, res) {
+  // Create a new instance of the Beer model
+  var beer = new Beer();
+
+  // Set the beer properties that came from the POST data
+  beer.name = req.body.name;
+  beer.type = req.body.type;
+  beer.quantity = req.body.quantity;
+
+  // Save the beer and check for errors
+  beer.save(function(err) {
+    if (err)
+      res.send(err);
+
+    res.json({ message: 'Beer added to the locker!', data: beer });
+  });
+});
+
+
+
 
 // Register all our routes with /api
 app.use('/api', router);
